f17639e520182db43380a27e1e663f30ad8bcb95
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-09-10 14:55:55 +0200

    Initial commit. Works well.

---
+++ env.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    var scion = require('scion');
+
+    var scxmlContent = $('#scxmlContent'),
+        scxmlLoadInitControls = $('#scxmlLoadInitControls'),
+        loadScxmlFromField = $('#loadScxmlFromField'),
+        initScxmlButton = $('#initScxmlButton'),
+        scxmlTrace = $('#scxmlTrace'),
+        scxmlSimulationControls = $('#scxmlSimulationControls'),
+        eventNameField = $('#eventNameField');
+
+    var scxmlInstance;
+
+    function trace(txt){
+        var p = document.createElement('p');
+        p.setAttribute('style','display:none');
+        $(p).text(txt);
+        scxmlTrace.append(p);
+        $(p).fadeIn();
+    }
+
+    var listener = {
+        onEntry : function(stateId){
+            trace('entering ' + stateId);
+        },
+        onExit : function(stateId){
+            trace('exiting ' + stateId);
+        }
+    };
+
+    //add behaviour
+    scxmlLoadInitControls.submit(function(e){
+        e.preventDefault();
+        var pathToScxml = loadScxmlFromField.val();
+        //AJAX GET it
+        //TODO: error handling
+        $.get(pathToScxml,function(scxmlText){
+            scxmlContent.val(scxmlText);
+        },"text");
+    });
+
+    initScxmlButton.click(function(){
+        //read the content and load it up
+        scion.documentStringToModel(scxmlContent.val(),function(err,model){
+            if(err){ 
+                alert(err.message);
+                throw err;
+            }
+
+            //clean up othe rinstance, if it exists
+            if(scxmlInstance){
+                scxmlInstance.unregisterListener(listener);
+            } 
+
+            scxmlInstance = new scion.SCXML(model);
+            scxmlInstance.registerListener(listener); 
+
+            var conf = scxmlInstance.start();
+            
+            trace('started new scxml instance >> ' + JSON.stringify(conf));
+        });
+    });
+
+    scxmlSimulationControls.submit(function(e){
+        e.preventDefault();
+
+        if(!scxmlInstance){
+            var err = new Error('SCXML interpreter not loaded');
+            alert(err.message);
+            throw err;
+        }
+
+        var eventName = eventNameField.val(); 
+        var conf = scxmlInstance.gen(eventName); 
+
+        trace(eventName + ' >> ' + JSON.stringify(conf));
+
+        eventNameField.val(''); 
+    }); 
+});
