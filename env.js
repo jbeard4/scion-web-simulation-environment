3a9b160bd7a9cd3f099a687b4991a1ffc8e07de3
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-10-03 22:35:40 -0400

    Added license text and cleaned up module dependencies.

--- env.js
+++ env.js
@@ -1,9 +1,23 @@
+/*
+     Copyright 2012-2013 Jacob Beard
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+             http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+
 $(document).ready(function(){
     var scion = require('scion');
 
     var scxmlContent = $('#scxmlContent'),
-        scxmlLoadInitControls = $('#scxmlLoadInitControls'),
-        loadScxmlFromField = $('#loadScxmlFromField'),
         initScxmlButton = $('#initScxmlButton'),
         scxmlTrace = $('#scxmlTrace'),
         scxmlSimulationControls = $('#scxmlSimulationControls'),
@@ -32,16 +46,6 @@ $(document).ready(function(){
     };
 
     //add behaviour
-    scxmlLoadInitControls.submit(function(e){
-        e.preventDefault();
-        var pathToScxml = loadScxmlFromField.val();
-        //AJAX GET it
-        //TODO: error handling
-        $.get(pathToScxml,function(scxmlText){
-            codeMirror.setValue(scxmlText);
-        },"text");
-    });
-
     function visualize(e){
         try {
             var doc = (new DOMParser()).parseFromString(codeMirror.getValue(),"application/xml");
